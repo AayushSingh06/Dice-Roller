a594390be78a60335f4144887de6a536d4cbbe80
AayushSingh06 <aayushsingh701@yahoo.in>
2022-01-20 10:00:49 +0530

    Dice roller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
          rollDice()
         }
+        diceImage=findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -25,7 +27,6 @@ class MainActivity : AppCompatActivity() {
             5->R.drawable.dice_5
             else->R.drawable.dice_6
         }
-        val diceImage: ImageView=findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
